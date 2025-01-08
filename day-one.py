ac5a89080474719d1e30dfc76b7295dc7f5fa10d
annabenari <annabenari@me.com>
2025-01-08 16:12:10 +0000

    Day one complete

--- day-one.py
+++ day-one.py
@@ -17,3 +17,28 @@ print("Hello World \nHello World \nHello World")
 
 # Concantenating
 print("Hello " + "World")
+
+# Inputs
+print("My name is Anna")
+otherName = input("What is your name? : ")
+print("Hello " + otherName)
+
+# Variables
+# everything in just 1 line of code.
+print(len(input("What is your name?")))
+# Split everything into variables.
+name = input("What is your name?")
+nameLength = len(name)
+print(nameLength)
+
+# We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice. Write 3 lines of code to switch the contents of the variables. You are not allowed to type the words "milk" or "juice". You are only allowed to use variables to solve this exercise.
+glass1 = "milk"
+glass2 = "juice"
+
+glass3 = glass1
+glass1 = glass2
+glass2 = glass3
+
+
+print(glass1)
+print(glass2)
