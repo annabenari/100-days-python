c913a70b075b48895f38c6e4f5bfe4753e747639
annabenari <annabenari@me.com>
2025-01-08 15:51:35 +0000

    More day 1 tasks

--- day-one.py
+++ day-one.py
@@ -4,3 +4,16 @@ print("Hello, welcome to the band genertor")
 lastName = input("What is your last name?: ")
 city = input("What city do you live in?: ")
 print("Your band name is " + city + lastName)
+
+# Make this into print statements
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+# Spliting the print stament into multiple lines
+print("Hello World \nHello World \nHello World")
+
+# Concantenating
+print("Hello " + "World")
