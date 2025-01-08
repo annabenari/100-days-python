a8983b8c914dc57024100f0149a3f2b996399913
annabenari <annabenari@me.com>
2025-01-08 15:41:36 +0000

    added notes day 1-4 and task day 1

---
+++ day-one.py
@@ -0,0 +1,6 @@
+# Print
+
+print("Hello, welcome to the band genertor")
+lastName = input("What is your last name?: ")
+city = input("What city do you live in?: ")
+print("Your band name is " + city + lastName)
