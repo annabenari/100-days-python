df1298cf86cd722e302f33bb8d1f6ff9437bb377
annabenari <annabenari@me.com>
2025-01-09 18:05:28 +0000

    Day 2 notes

--- day-two.py
+++ day-two.py
@@ -12,3 +12,17 @@ print(123.456)
 # Boolean = only two values
 print(True)
 print(False)
+
+# Type
+print(type("String"))
+print(type(1234))
+print(type(123.456))
+print(type(True))
+
+# <class 'str'>
+# <class 'int'>
+# <class 'float'>
+# <class 'bool'>
+
+# print("Number of letters in your name: " + len(input("Enter your name"))) --> Comes up with error , cannot concatenate a str and int. The len(input("Enter your name"))) is an int therefore chnage to str.
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
