b7c3f8424ccb3d7087d8f8e6e8fa2768f30a3da4
annabenari <annabenari@me.com>
2025-01-09 15:13:34 +0000

    Day 2 notes

---
+++ day-two.py
@@ -0,0 +1,14 @@
+len("Hello")
+# Subscripting
+print("HELLO"[2])
+# String
+print("123" + "345")  # This just concatinates the two strings
+# Integar = whole number
+print(123 + 234)
+# large Integar
+print(123_234_234)
+# Float = Floating point number (decimal point)
+print(123.456)
+# Boolean = only two values
+print(True)
+print(False)
