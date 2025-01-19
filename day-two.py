0ac1ccb9a8d89b760ab4f744da844530577ab819
annabenari <annabenari@me.com>
2025-01-19 16:23:34 +0000

    day two tip calculator

--- day-two.py
+++ day-two.py
@@ -86,3 +86,15 @@ print(f"I am {age} years old")
 print(6 + 4 / 2 - (1 * 2))
 6+4/2 - (2)
 6+2 - 2
+
+# Tip calculator:
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+
+perPerson = (bill / people) * ((tip/100) + 1)
+
+perPerson = round(perPerson, 2)  # Round to 2 decimal places
+print(f"Each person should pay: ${perPerson}"
